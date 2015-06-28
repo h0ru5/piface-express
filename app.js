7918412e52518b61491080b392995d42c30b126c
h0ru5 <johannes.hund@gmail.com>
2015-06-28 23:35:11 +0200

    split up express routes and primus

--- app.js
+++ app.js
@@ -1,104 +1,30 @@
 var pfio = require('pfio');
-var express = require('express');
 var http = require("http");
 var Primus = require("primus");
-var bodyParser = require('body-parser');
-var app = express();
+var app = require("./express-setup");
 
-var byte2bit = function(byte, flip) {
-  var res = [];
-  for (var i = 0; i < 8; i++) {
-    res[i] = (byte & (1 << i)) === (1 << i);
-    if (flip) res[i] = !res[i];
-  }
-  return res;
-};
-
-app.use('/static', express.static('static'));
-app.use(bodyParser.json());
-
-app.get('/', function(req, res) {
-  //should use HATEOAS here
-  res.send('basic piface service use /inputs or /outputs');
-});
-
-app.get('/inputs', function(req, res) {
-  var regbyte = pfio.read_input();
-  var states = byte2bit(regbyte, true);
-  res.json({
-    'register': regbyte,
-    'state': states
-  });
-});
-
-app.get('/inputs/:id', function(req, res) {
-  var states = byte2bit(pfio.read_input(), true);
-  var pin = req.params.id;
-  res.json({
-    'pin': pin,
-    'value': states[pin]
-  });
-});
-
-app.route('/outputs')
-  .get(function(req, res) {
-    var regbyte = pfio.read_output();
-    res.json({
-      'register': regbyte,
-      'state': byte2bit(regbyte)
-    });
-  })
-  .put(function(req, res) {
-    if (req.body.hasOwnProperty('register')) {
-      pfio.write_output(req.body.register);
-      res.end();
-    } else {
-      res.status(400).send("Missing key 'register' in " + JSON.stringify(req.body));
-    }
-  });
-
-app.route('/outputs/:id')
-  .get(function(req, res) {
-    var states = byte2bit(pfio.read_output());
-    var pin = req.params.id;
-    res.json({
-      'pin': pin,
-      'value': states[pin]
-    });
-  })
-  .put(function(req, res) {
-    var pin = req.params.id;
-    if (req.body.hasOwnProperty('value')) {
-      pfio.digital_write(pin, req.body.value);
-      res.end();
-    } else {
-      res.status(400).send("Missing key 'value' in " + JSON.stringify(req.body));
-    }
-  });
-
-  var server = require('http').createServer(app);
-  var  primus = new Primus(server);
+var server = require('http').createServer(app);
+var primus = new Primus(server);
 
 var observers = [];
 
-primus.on('connection', function (spark) {
-    observers.push(spark);
-  });
+primus.on('connection', function(spark) {
+  observers.push(spark);
+});
 
-primus.on('disconnection', function (spark) {
+primus.on('disconnection', function(spark) {
   var idx = observers.indexOf(spark);
-  if(idx !=-1) {
-    observers.splice(idx,1);
+  if (idx != -1) {
+    observers.splice(idx, 1);
   }
 });
+
+//watch for changes and notify observers
 
 server.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
-
   pfio.init();
   console.log('Piface app listening at http://%s:%s', host, port);
-
   process.on('SIGINT', pfio.deinit);
-
 });
---
+++ express-setup.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var app = express();
+var pfio = require('pfio');
+var bodyParser = require('body-parser');
+
+var byte2bit = function(byte, flip) {
+  var res = [];
+  for (var i = 0; i < 8; i++) {
+    res[i] = (byte & (1 << i)) === (1 << i);
+    if (flip) res[i] = !res[i];
+  }
+  return res;
+};
+
+app.byte2bit = byte2bit;
+
+app.use('/static', express.static('static'));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  //should use HATEOAS here
+  res.send('basic piface service use /inputs or /outputs');
+});
+
+app.get('/inputs', function(req, res) {
+  var regbyte = pfio.read_input();
+  var states = byte2bit(regbyte, true);
+  res.json({
+    'register': regbyte,
+    'state': states
+  });
+});
+
+app.get('/inputs/:id', function(req, res) {
+  var states = byte2bit(pfio.read_input(), true);
+  var pin = req.params.id;
+  res.json({
+    'pin': pin,
+    'value': states[pin]
+  });
+});
+
+app.route('/outputs')
+  .get(function(req, res) {
+    var regbyte = pfio.read_output();
+    res.json({
+      'register': regbyte,
+      'state': byte2bit(regbyte)
+    });
+  })
+  .put(function(req, res) {
+    if (req.body.hasOwnProperty('register')) {
+      pfio.write_output(req.body.register);
+      res.end();
+    } else {
+      res.status(400).send("Missing key 'register' in " + JSON.stringify(req.body));
+    }
+  });
+
+app.route('/outputs/:id')
+  .get(function(req, res) {
+    var states = byte2bit(pfio.read_output());
+    var pin = req.params.id;
+    res.json({
+      'pin': pin,
+      'value': states[pin]
+    });
+  })
+  .put(function(req, res) {
+    var pin = req.params.id;
+    if (req.body.hasOwnProperty('value')) {
+      pfio.digital_write(pin, req.body.value);
+      res.end();
+    } else {
+      res.status(400).send("Missing key 'value' in " + JSON.stringify(req.body));
+    }
+  });
+
+module.exports = app;
