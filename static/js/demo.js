372bb92d3aa3229b0ee4f489d96c1844aced8d67
Johannes Hund (h0ru5) <johannes.hund@gmail.com>
2015-08-16 22:10:46 +0200

    demo for angularesque view

--- static/js/demo.js
+++ static/js/demo.js
@@ -1,18 +1,34 @@
 angular.module('piface-demo', ['piface'])
-  .controller('PiFaceCtrl', ["$scope", 'piface',
-    function($scope, piface) {
-      $scope.data = {};
-      piface.watchInputs(function(inputs) {
-        $scope.data = inputs;
-      });
+    .controller('PiFaceCtrl', ["$scope", 'piface',
+        function($scope, piface) {
+            $scope.inputs = {};
+            $scope.outputs = [];
 
-      $scope.clearOutputs = function() {
-        piface.setOutputs(0x00);
-      };
+            for (var i=0;i<8;i++) {
+                $scope.outputs.push(
+                    {
+                        "number": i,
+                        "value" : false,
+                        "setTo" : function(nV) {
+                            this.value=nV;
+                            console.log("setting output " + this.number + " to " + this.value);
+                            piface.setSingleOutput(this.number,this.value);
+                        }
+                    }
+                );
+            }
 
-      $scope.randomHigh = function() {
-        var rnd_0to7 = Math.floor((Math.random() * 7) + 1);
-        piface.setSingleOutput(rnd_0to7, true);
-      };
-    }
-  ]);
+            piface.watchInputs(function(inputs) {
+                $scope.inputs = inputs;
+            });
+
+            $scope.clearOutputs = function() {
+                piface.setOutputs(0x00);
+            };
+
+            $scope.randomHigh = function() {
+                var rnd_0to7 = Math.floor((Math.random() * 7) + 1);
+                piface.setSingleOutput(rnd_0to7, true);
+            };
+        }
+    ]);
